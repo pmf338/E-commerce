03b7bf33e5f0be84a3c81ab9cbd50ba8e6a09ddf
pmf338 <pmf338@gmail.com>
2023-04-02 14:41:10 -0300

    Ajustes de diseño con boostrap en login.ejs,createProfile.ejs y contact.ejs

--- src/database/models/products.js
+++ src/database/models/products.js
@@ -40,12 +40,6 @@ module.exports = (sequelize, dataTypes) => {
             allowNull: true
         },
 
-        category: {
-
-            type: dataTypes.STRING(45),
-            allowNull: false
-        },
-
         imagesUrl: {
 
             type: dataTypes.STRING(350),
