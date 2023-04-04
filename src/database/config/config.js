d3acc16f0dc6de2e10a65f002e576746439397c1
SNardillo <larry.nar@hotmail.com>
2023-04-03 21:42:57 -0300

    Imagenes de Artistas

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,23 +2,23 @@
  {
   "development": {
     "username": "root",
-    "password": "root",
-    "database": "rocket_music",
+    "password": "basededatos123",
+    "database": "mydb",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": "3306"
   },
   "test": {
     "username": "root",
-    "password": "root",
-    "database": "rocket_music",
+    "password": "basededatos123",
+    "database": "mydb",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "root",
-    "database": "rocket_music",
+    "password": "basededatos123",
+    "database": "mydb",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
